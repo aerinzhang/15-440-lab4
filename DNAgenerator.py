14b8e0f030c7dd6958c5ad1a3c85ee8f317d0287
aerinzhang <shikunz@andrew.cmu.edu>
2013-05-03 23:27:58 -0400

    new pointGen

--- DNAgenerator.py
+++ DNAgenerator.py
@@ -10,13 +10,69 @@ optparser.add_option("-o", "--output", dest="output", default="data/output.txt",
 optparser.add_option("-l", "--length-of-DNA", dest="dLength", default=20, type="int", help="length of DNA strand")
 (opts, _) = optparser.parse_args()
 
-def DNAgenerator(outfile, p, k):
-	o = open(outfile, "w+")
-	bases = ["A", "C", "G", "T"]
-	for i in xrange(p*k):
-		seq = []
-		for j in xrange(20):
-			seq += [bases[random.randint(0, 3)]]
-		o.write(''.join(seq)+'\n')
+def createCentroids(k, l):
+	cList = []
+	radius = int(r/(k**(0.5)))
+	while (len(cList) < k):
+		newCentroid = ((random.random()*r, random.random()*r),
+					int(random.random()*radius))
+		if (checkCentroids(cList, newCentroid)):
+			cList += [newCentroid]
+	return cList
 
-DNAgenerator(opts.output, opts.pValue, opts.kValue)
+def checkTwoCentroids(c1, c2):
+	((x1, y1), r1) = c1
+	((x2, y2), r2) = c2
+	if (((x1-x2)**2 + (y1-y2)**2))**(0.5) <= r1+r2:
+		return False
+	else:
+		return True
+
+def checkCentroids(known, new):
+	for each in known:
+		#if two centroids are too close
+		if checkTwoCentroids(each, new):
+			return False
+	return True
+
+def pointsAroundCentroid(c, p):
+	pointSet = set()
+	((x, y), r) = c
+	while (len(pointSet) < p):
+		angle = random.random()*(2*math.pi)
+		rad = random.random()*r
+		newPoint = (x+rad*math.sin(angle), y+rad*math.cos(angle))
+		pointSet.add(newPoint)
+	return pointSet
+
+def similarityOfStrands(d1, d2):
+	sim = 0
+	for i in xrange(len(d1)):
+		if (d1[i] == d2[i]):
+			sim += 1
+	return sim
+
+def createCentroids(k, l):
+	cList = []
+	diffRad = random.randint(1, l)
+	while(len(cList) < k):
+		newCentroid = (createStrand(l), diffRad)
+		if ():
+			cList += [newCentroid]
+	return cList
+
+def createStrand(l):
+	bases = ["A", "C", "G", "T"]
+	seq = []
+	for i in xrange(l):
+		seq += [bases[random.randint(0, 3)]]
+	return ''.join(seq)
+
+	[bases[random.randint(0, 3)]]
+def DNAGenerator(outfile, p, k):
+	o = open(outfile, "w+")
+	for i in xrange(p*k):
+		o.write(''.join(seq)+'\n')
+	o.close()
+	return 42
+DNAGenerator(opts.output, opts.pValue, opts.kValue)
--- PointGenerator.py
+++ PointGenerator.py
@@ -2,6 +2,7 @@
 import optparse
 import sys
 import random
+import math
 
 optparser = optparse.OptionParser()
 optparser.add_option("-k", "--value-of-k", dest="kValue", type="int", help="number of clusters")
@@ -10,10 +11,55 @@ optparser.add_option("-o", "--output", dest="output", help="location of output f
 optparser.add_option("-r", "--range", dest="range", default=10000, type="int", help="range of coordinates")
 (opts, _) = optparser.parse_args()
 
+
+def createCentroids(k, r):
+	cList = []
+	radius = int(r/(k**(0.5)))
+	while (len(cList) < k):
+		newCentroid = ((random.random()*r, random.random()*r),
+					int(random.random()*radius))
+		if (checkCentroids(cList, newCentroid)):
+			cList += [newCentroid]
+	return cList
+
+def checkTwoCentroids(c1, c2):
+	((x1, y1), r1) = c1
+	((x2, y2), r2) = c2
+	if (((x1-x2)**2 + (y1-y2)**2))**(0.5) <= r1+r2:
+		return False
+	else:
+		return True
+
+def checkCentroids(known, new):
+	for each in known:
+		#if two centroids are too close
+		if checkTwoCentroids(each, new):
+			return False
+	return True
+
+def pointsAroundCentroid(c, p):
+	pointSet = set()
+	((x, y), r) = c
+	while (len(pointSet) < p):
+		angle = random.random()*(2*math.pi)
+		rad = random.random()*r
+		newPoint = (x+rad*math.sin(angle), y+rad*math.cos(angle))
+		pointSet.add(newPoint)
+	return pointSet
+
+
+
 def PointGenerator(outfile, p, k, r):
 	o = open(outfile, "w+")
-	for i in xrange(p*k):
-		(x,y) = (random.randint(0, r), random.randint(0, r))
-		o.write("%d %d\n"%(x,y))
-
+	# generate centroids list
+	cList = createCentroids(k, r)
+	for i in xrange(k):
+		#for each centroids, generate p data points
+		c = cList[i]
+		pSet = pointsAroundCentroid(c, p)
+		while (len(pSet) != 0):
+			(x, y) = pSet.pop()
+			o.write("%f %f\n"%(x,y))
+	o.close()
+	return 42
 PointGenerator(opts.output, opts.pValue, opts.kValue, opts.range)
